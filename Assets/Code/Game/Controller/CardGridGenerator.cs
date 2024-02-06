ea6c99d20f874f27ab595f89f4945fe4f3d5164f
Akshay Kumar <akshayk773@gmail.com>
2024-02-06 22:41:24 +0530

    Added card grid generating logic

--- Assets/Code/Game/Controller/CardGridGenerator.cs
+++ Assets/Code/Game/Controller/CardGridGenerator.cs
@@ -4,6 +4,35 @@ namespace Code.Game.Controller
 {
     public class CardGridGenerator : MonoBehaviour
     {
-    
+        [SerializeField] private GameObject cardPrefab;
+        [SerializeField] private int rows;
+        [SerializeField] private int columns;
+        [SerializeField] private float cardSpacing = 1.6f;
+
+        private void Start()
+        {
+            GenerateCardGrid();
+        }
+
+        private void GenerateCardGrid()
+        {
+            var gridWidth = columns * cardSpacing;
+            var gridHeight = rows * cardSpacing;
+
+            var startX = -gridWidth / 2 + cardSpacing / 2;
+            var startY = gridHeight / 2 - cardSpacing / 2;
+
+            for (var row = 0; row < rows; row++)
+            for (var col = 0; col < columns; col++)
+            {
+                var posX = startX + col * cardSpacing;
+                var posY = startY - row * cardSpacing;
+
+                var position = new Vector3(posX, posY, 0f);
+                var rotation = Quaternion.identity;
+
+                var card = Instantiate(cardPrefab, position, rotation);
+            }
+        }
     }
 }
