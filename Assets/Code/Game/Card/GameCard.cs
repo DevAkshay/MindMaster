73ed8fd1d48b1e247eceaf364f680af73108e2e2
Akshay Kumar <akshayk773@gmail.com>
2024-02-07 21:31:30 +0530

    Fixed Card flipping animation issue

--- Assets/Code/Game/Card/GameCard.cs
+++ Assets/Code/Game/Card/GameCard.cs
@@ -9,23 +9,22 @@ namespace Code.Game.Card
     {
         [SerializeField] private SpriteRenderer card;
         [SerializeField] private SpriteRenderer icon;
-        
+
         [SerializeField] private float flipDuration = 0.2f;
         [SerializeField] private AnimationCurve flipCurve = AnimationCurve.Linear(0, 0, 1, 1);
-        
+
         [SerializeField] private Sprite cardBackSprite;
         [SerializeField] private Sprite cardFrontSprite;
 
         public Action<GameCard> OnCardClick;
-        
-        public bool IsMatched { get; private set; }
 
-        private Quaternion _startRotation;
-        private Quaternion _endRotation;
+        public bool IsMatched { get; private set; }
 
         private bool _isFlipping;
         private bool _isFrontSideVisible; // Flag to track if the front side of the card is visible
 
+        private Quaternion _halfFlipRotation;
+        private Quaternion _fullFlipRotation;
 
         private void OnMouseDown()
         {
@@ -36,7 +35,7 @@ namespace Code.Game.Card
         {
             if (_isFlipping)
                 return;
-            
+
             // Start flipping animation
             StartCoroutine(AnimateFlip());
         }
@@ -59,56 +58,44 @@ namespace Code.Game.Card
         public void SetAsMatched()
         {
             IsMatched = true;
-            
+
             Pool.ReleaseObject(gameObject);
         }
 
         private IEnumerator AnimateFlip()
         {
-            _isFlipping = true;
-            var elapsedTime = 0f;
+            _halfFlipRotation = Quaternion.Euler(0, 90, 0);
+            _fullFlipRotation = Quaternion.Euler(0, 180, 0);
 
-            _startRotation = transform.rotation;
-            _endRotation = _startRotation * Quaternion.Euler(0, 90, 0);
-
-            _isFrontSideVisible = !_isFrontSideVisible;
-
-            while (elapsedTime < flipDuration)
+            for (var i = 0; i < 2; i++)
             {
-                var t = flipCurve.Evaluate(elapsedTime / flipDuration);
-                transform.rotation = Quaternion.Slerp(_startRotation, _endRotation, t);
+                var elapsedTime = 0f;
+                var startRotation = transform.rotation;
+                var endRotation = i == 0 ? _halfFlipRotation : _fullFlipRotation;
 
-                if (t >= 0.97f)
+                while (elapsedTime < flipDuration)
                 {
-                    // Switch card sprite to front side if it's visible, otherwise switch to back side
-                    card.sprite = _isFrontSideVisible ? cardFrontSprite : cardBackSprite;
-                    icon.gameObject.SetActive(_isFrontSideVisible);
+                    elapsedTime += Time.deltaTime;
+                    var t = flipCurve.Evaluate(elapsedTime / flipDuration);
+                    transform.rotation = Quaternion.Lerp(startRotation, endRotation, t);
+
+                    if (i == 0 && t >= 0.5f) // Midway, change the sprite and icon visibility
+                    {
+                        _isFrontSideVisible = !_isFrontSideVisible;
+                        card.sprite = _isFrontSideVisible ? cardFrontSprite : cardBackSprite;
+                        icon.gameObject.SetActive(_isFrontSideVisible);
+                        break; // Break the loop and continue to the next iteration for the second half of the flip
+                    }
+
+                    yield return null;
                 }
 
-                elapsedTime += Time.deltaTime;
-                yield return null;
+                transform.rotation = endRotation;
             }
 
-            transform.rotation = _endRotation;
-
-            // Set the start and end rotations for the second half of the flip
-            _startRotation = _endRotation;
-            _endRotation = _startRotation * Quaternion.Euler(0, -90, 0);
-            elapsedTime = 0f;
-
-            while (elapsedTime < flipDuration)
-            {
-                var t = flipCurve.Evaluate(elapsedTime / flipDuration);
-                transform.rotation = Quaternion.Slerp(_startRotation, _endRotation, t);
-
-                elapsedTime += Time.deltaTime;
-                yield return null;
-            }
-
-            transform.rotation = _endRotation;
             _isFlipping = false;
-
         }
+
 
         public void OnObjectInit()
         {
